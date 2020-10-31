7340094f7ae6da3b3ed17da6c0d5317189865040
shantaram8 <dont.give.me.sf@gmail.com>
2020-10-31 14:10:52 +0600

    Added markup

--- app/src/main/java/kz/kolesateam/confapp/MainActivity.kt
+++ app/src/main/java/kz/kolesateam/confapp/MainActivity.kt
@@ -1,10 +1,9 @@
 package kz.kolesateam.confapp
 
 import android.content.Intent
-import androidx.appcompat.app.AppCompatActivity
 import android.os.Bundle
-import android.util.Log
 import android.widget.Button
+import androidx.appcompat.app.AppCompatActivity
 import kz.kolesateam.confapp.hello.presentation.HelloActivity
 
 private const val TAG = "MainActivity"
@@ -12,7 +11,7 @@ private const val TAG = "MainActivity"
 class MainActivity : AppCompatActivity() {
 
     private val openHelloButton: Button by lazy {
-        findViewById(R.id.activity_main_open_hello_button)
+        findViewById(R.id.activity_main_continue_button)
     }
 
     override fun onCreate(savedInstanceState: Bundle?) {
@@ -24,42 +23,5 @@ class MainActivity : AppCompatActivity() {
             startActivity(helloScreenIntent)
         }
 
-        Log.d(TAG, "onCreate")
-    }
-
-    override fun onRestart() {
-        super.onRestart()
-
-        Log.d(TAG, "onRestart")
-    }
-
-    override fun onStart() {
-        super.onStart()
-
-        Log.d(TAG, "onStart")
-    }
-
-    override fun onResume() {
-        super.onResume()
-
-        Log.d(TAG, "onResume")
-    }
-
-    override fun onPause() {
-        Log.d(TAG, "onPause")
-
-        super.onPause()
-    }
-
-    override fun onStop() {
-        Log.d(TAG, "onStop")
-
-        super.onStop()
-    }
-
-    override fun onDestroy() {
-        Log.d(TAG, "onDestroy")
-
-        super.onDestroy()
     }
 }

b3bc4adcc81bae32e96979168bf38c39ec52c5bf
shantaram8 <dont.give.me.sf@gmail.com>
2020-11-09 05:59:05 +0600

    Created ApiClient

--- app/src/main/java/kz/kolesateam/confapp/events/presentation/UpcomingEventsActivity.kt
+++ app/src/main/java/kz/kolesateam/confapp/events/presentation/UpcomingEventsActivity.kt
@@ -5,6 +5,17 @@ import android.widget.Button
 import android.widget.TextView
 import androidx.appcompat.app.AppCompatActivity
 import kz.kolesateam.confapp.R
+import kz.kolesateam.confapp.events.data.ApiClient
+import retrofit2.Retrofit
+import retrofit2.converter.jackson.JacksonConverterFactory
+
+
+val apiRetrofit: Retrofit = Retrofit.Builder()
+    .baseUrl("http://37.143.8.68:2020")
+    .addConverterFactory(JacksonConverterFactory.create())
+    .build()
+
+val apiClient: ApiClient = apiRetrofit.create(ApiClient::class.java)
 
 class UpcomingEventsActivity : AppCompatActivity() {
     override fun onCreate(savedInstanceState: Bundle?) {

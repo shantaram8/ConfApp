a11a0d7ba7e369737768e9a3b37f0a113b473843
shantaram8 <dont.give.me.sf@gmail.com>
2020-11-09 13:43:23 +0600

    Added progressBar

--- app/src/main/java/kz/kolesateam/confapp/events/presentation/UpcomingEventsActivity.kt
+++ app/src/main/java/kz/kolesateam/confapp/events/presentation/UpcomingEventsActivity.kt
@@ -1,7 +1,9 @@
 package kz.kolesateam.confapp.events.presentation
 
 import android.os.Bundle
+import android.view.View
 import android.widget.Button
+import android.widget.ProgressBar
 import android.widget.TextView
 import androidx.appcompat.app.AppCompatActivity
 import com.fasterxml.jackson.databind.JsonNode
@@ -26,12 +28,15 @@ class UpcomingEventsActivity : AppCompatActivity() {
     private lateinit var jsonResponse: TextView
     private lateinit var asyncButton: Button
     private lateinit var syncButton: Button
+    private lateinit var progressBar: ProgressBar
+
 
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
         setContentView(R.layout.activity_upcoming_events)
 
         bindViews()
+        progressBar.visibility = View.GONE
         asyncButton.setOnClickListener {
             loadApiDataAsync()
         }
@@ -45,32 +50,38 @@ class UpcomingEventsActivity : AppCompatActivity() {
         jsonResponse = findViewById(R.id.activity_upcoming_events_text_view)
         asyncButton = findViewById(R.id.activity_upcoming_events_async_button)
         syncButton = findViewById(R.id.activity_upcoming_events_sync_button)
+        progressBar = findViewById(R.id.activity_upcoming_events_progress_bar)
     }
 
     private fun loadApiDataAsync() {
-        jsonResponse.setTextColor(resources.getColor(R.color.upcoming_events_activity_text_view_color_async))
-
+        progressBar.visibility = View.VISIBLE
         apiClient.getUpcomingEvents().enqueue(object : Callback<JsonNode> {
             override fun onResponse(call: Call<JsonNode>, response: Response<JsonNode>) {
                 if (response.isSuccessful) {
                     val body: JsonNode = response.body()!!
                     jsonResponse.text = body.toString()
+                    jsonResponse.setTextColor(resources.getColor(R.color.upcoming_events_activity_text_view_color_async))
                 }
             }
 
             override fun onFailure(call: Call<JsonNode>, t: Throwable) {
-                jsonResponse.setTextColor(resources.getColor(R.color.upcoming_events_activity_text_view_error_color))
                 jsonResponse.text = t.localizedMessage
+                jsonResponse.setTextColor(resources.getColor(R.color.upcoming_events_activity_text_view_error_color))
             }
         })
     }
 
     private fun loadApiDataSync() {
         Thread {
+            Thread.sleep(10)
             try {
+                runOnUiThread{
+                    progressBar.visibility = View.VISIBLE
+                }
                 val response: Response<JsonNode> = apiClient.getUpcomingEvents().execute()
                 val body: JsonNode = response.body()!!
                 runOnUiThread {
+                    progressBar.visibility = View.GONE
                     jsonResponse.text = body.toString()
                     jsonResponse.setTextColor(resources.getColor(R.color.upcoming_events_activity_text_view_color_sync))
                 }

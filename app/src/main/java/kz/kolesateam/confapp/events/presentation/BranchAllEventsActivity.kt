d02692137c49c2e1137145c785385da7c7a17a33
Lavrentyev Anton <dont.give.me.sf@gmail.com>
2020-11-29 20:26:55 +0600

    Style (activity_branch_all_events.xml) : Created RecyclerView

--- app/src/main/java/kz/kolesateam/confapp/events/presentation/BranchAllEventsActivity.kt
+++ app/src/main/java/kz/kolesateam/confapp/events/presentation/BranchAllEventsActivity.kt
@@ -1,4 +1,13 @@
 package kz.kolesateam.confapp.events.presentation
 
-class BranchAllEventsActivity {
+import android.os.Bundle
+import androidx.appcompat.app.AppCompatActivity
+import kz.kolesateam.confapp.R
+
+class BranchAllEventsActivity : AppCompatActivity() {
+    override fun onCreate(savedInstanceState: Bundle?) {
+        super.onCreate(savedInstanceState)
+        setContentView(R.layout.activity_branch_all_events)
+
+    }
 }

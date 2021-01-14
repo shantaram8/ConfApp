3eb715488a7a4490135eae4789df3bbd03f385dc
Lavrentyev Anton <dont.give.me.sf@gmail.com>
2021-01-14 13:30:52 +0600

    Feat : Changed app icon

--- app/src/main/java/kz/kolesateam/confapp/MainActivity.kt
+++ app/src/main/java/kz/kolesateam/confapp/MainActivity.kt
@@ -28,6 +28,7 @@ class MainActivity : AppCompatActivity() {
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
         setContentView(R.layout.activity_main)
+
         if (!userNameDataSource.getSavedUserName().isNullOrEmpty()) {
             navigateToUpcomingEventsScreen()
         }
